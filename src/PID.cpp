3b8833a9660a86a0d2cfc5947c6cb6387af158e7
lake4790k <lake4790k@users.noreply.github.com>
2017-08-24 07:02:55 +0200

    pid implementation

--- src/PID.cpp
+++ src/PID.cpp
@@ -1,21 +1,32 @@
 #include "PID.h"
 
-using namespace std;
+#include <math.h>
 
-/*
-* TODO: Complete the PID class.
-*/
 
 PID::PID() {}
 
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+    this->Kd = Kd;
+    this->Kp = Kp;
+    this->Ki = Ki;
 }
 
 void PID::UpdateError(double cte) {
+    d_error = cte - p_error;
+    p_error = cte;
+    i_error += cte;
+}
+
+double PID::cappedControl() {
+    double control = Kp * p_error + Kd * d_error + Ki * i_error;
+    if (control > 1) control = 1;
+    if (control < -1) control = -1;
+    return control;
 }
 
 double PID::TotalError() {
+    return i_error;
 }
 
